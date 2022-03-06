59b3822695f0a3f03d7babc2cb1fe12ace0b389b
Helson Matos <helson.11@hotmail.com>
2022-03-06 10:54:42 -0300

    aula 2

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let order = [];;
+let clickedOrder = [];
+let score = 0;
+
+// 0 = green
+// 1 = red
+// 2 = blue
+// 3 = yellow
+
+const green = document.querySelector(".green");
+const red = document.querySelector(".red");
+const blue = document.querySelector(".blue");
+const yellow = document.querySelector(".yellow");
